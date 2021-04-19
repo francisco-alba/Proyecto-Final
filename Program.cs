a34ddc52006b3b5a1eae770400d16cbd4ad2f9d0
Perla-Alejandra-Soriano-Velasco <perla.soriano@my.unitec.edu.mx>
2021-04-19 18:47:18 -0500

    BD2

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace cli
+{
+    class Program
+    {
+        private readonly static string connectionString = "Data Source=****;Initial Catalog=****;User ID=****;Password=****;";
+        static void Main(string[] args)
+        {
+            // Insertar();
+            // Eliminar();
+             Mostrar();
+        }
+        static void Insertar()
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("INSERT INTO [Empleados] ([Id],[Nombre],[Edad]) VALUES (NEWID(),'Pepe Pepe',15);", con);
+            cmd.ExecuteNonQuery();
+
+            Console.WriteLine("Insertado");
+
+            con.Close();
+        }
+        static void Eliminar()
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("DELETE FROM [Empleados] WHERE [Id] = 'e0267f65-b7e5-4942-9125-ace950f1363';", con);
+            cmd.ExecuteNonQuery();
+
+            Console.WriteLine("Eliminado");
+
+            con.Close();
+        }
+        static void Mostrar()
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("SELECT [Id], [Nombre], [Edad] FROM [Empleados];", con);
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                Console.WriteLine("Id: " + dr["Id"]);
+                Console.WriteLine("Nombre: " + dr["Nombre"]);
+                Console.WriteLine("Edad: " + dr["Edad"]);
+            }
+
+            con.Close();
+        }
+    }
+}
